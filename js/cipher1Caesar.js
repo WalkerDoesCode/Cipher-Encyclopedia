c99871e783077b67926bb41019973a9e78ed7f1f
WalkerDoesCode <walker.kroubalkian@gmail.com>
2020-09-14 23:32:44 -0700

    First Commit: Javascript Encrypter/Decrypter for Cipher #1: The Caesar Cipher

---
+++ js/cipher1Caesar.js
@@ -0,0 +1,57 @@
+function myCaesarCipher() {
+    var t = document.getElementById("cipherText");
+    var tString = t.value;
+
+    var k = document.getElementById("cipherKey");
+    var kString = k.value;
+    var kNum = parseInt(kString, 10);
+
+    var operationInputs = document.getElementsByName("cipherOperation");
+    var text = "";
+    var temp;
+    var cipherOperation = "0";
+
+    for(i = 0; i<operationInputs.length; i++) {
+        temp = operationInputs[i];
+        if(temp.type == "radio" && temp.checked) {
+            cipherOperation = temp.value;
+        };
+    };
+
+    if(cipherOperation == "1") {
+        text += "Decrypt ciphertext: ";
+    } else {
+        text += "Encrypt plaintext: ";
+    };
+
+    text += tString + "<br><br>";
+    text += "Key = " + kString + "<br><br>";
+
+    if(cipherOperation == "1") {
+        text += "Corresponding plaintext: ";
+        text += decryptCaesar(tString, kNum);
+    } else {
+        text += "Corresponding ciphertext: ";
+        text += decryptCaesar(tString, -kNum);
+    }
+
+    document.getElementById("demo").innerHTML = text;
+};
+
+function decryptCaesar(cipherText, key) {
+    var text = "";
+    var character = 0;
+    var newAscii = 0;
+    for(i = 0; i<cipherText.length; i++) {
+        character = cipherText.charCodeAt(i);
+        if(65<=character && character<=90) {
+            newAscii = (character-key+39)%26 + 65;
+        } else if(97<=character && character<=122) {
+            newAscii = (character-key+33)%26 + 97;
+        } else {
+            return "6597 ERROR";
+        }
+        text += String.fromCharCode(newAscii);
+    }
+    return text;
+}
